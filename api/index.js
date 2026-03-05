7f92263447cd0a9e9d3a94a56b5469dd79d89f0c
Soumya Chakraborty <soumya.chk101@gmail.com>
2026-03-06 04:38:33 +0530

    Fix 500 Error: Disable Vercel bodyParser so multer can read multipart streams

--- api/index.js
+++ api/index.js
@@ -1,3 +1,10 @@
 const app = require('../src/app');
 
 module.exports = app;
+
+// Disable Vercel's default body parser so multer can handle multipart/form-data
+module.exports.config = {
+    api: {
+        bodyParser: false,
+    },
+};
